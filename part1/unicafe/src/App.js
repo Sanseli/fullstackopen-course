defa6fa61fbe2deb51631643a8ddcbd9b6cc6731
Sanseli <lisasansen97@gmail.com>
2023-02-03 17:26:55 +0200

    1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ title, onClick }) => <button onClick={onClick}>{title}</button>
+
+const Statistic = ({ title, value }) => (
+	<p>
+		{title} {value}
+	</p>
+)
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	return (
+		<>
+			<h1>give feedback</h1>
+			<Button title='good' onClick={() => setGood(good + 1)} />
+			<Button title='neutral' onClick={() => setNeutral(neutral + 1)} />
+			<Button title='bad' onClick={() => setBad(bad + 1)} />
+
+			<h1>statistics</h1>
+			<Statistic title='good' value={good} />
+			<Statistic title='neutral' value={neutral} />
+			<Statistic title='bad' value={bad} />
+		</>
+	)
 }
 
-export default App;
+export default App
