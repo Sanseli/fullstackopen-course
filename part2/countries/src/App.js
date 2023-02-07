7d9ea8a789cd2bf21d16253fd3c9b68a6e61e500
Sanseli <lisasansen97@gmail.com>
2023-02-07 19:06:36 +0200

    part2 countries init

---
+++ part2/countries/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div>
+			<div>
+				find countries <input />
+			</div>
+			<div>too many matches, specify another filter</div>
+		</div>
+	)
+}
+
+export default App
