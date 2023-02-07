d15982dd3e667ccd2a1bf6d073e3adc1cd7110ad
Sanseli <lisasansen97@gmail.com>
2023-02-07 18:29:05 +0200

    Adding styles to React app

--- part2/example/src/App.js
+++ part2/example/src/App.js
@@ -1,12 +1,34 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+
+const Notification = ({ message }) => {
+	if (message === null) {
+		return null
+	}
+
+	return <div className='error'>{message}</div>
+}
+
+const Footer = () => {
+	const footerStyle = {
+		color: 'green',
+		fontStyle: 'italic',
+		fontSize: 16,
+	}
+	return (
+		<div style={footerStyle}>
+			<br />
+			<em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+		</div>
+	)
+}
 
 const App = () => {
 	const [notes, setNotes] = useState([])
 	const [newNote, setNewNote] = useState('a new note...')
 	const [showAll, setShowAll] = useState(true)
+	const [errorMessage, setErrorMessage] = useState('some error happened...')
 
 	useEffect(() => {
 		noteService.getAll().then((initialNotes) => {
@@ -37,7 +59,10 @@ const App = () => {
 				setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
 			})
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from server`)
+				setErrorMessage(`Note '${note.content}' was already removed from server`)
+				setTimeout(() => {
+					setErrorMessage(null)
+				}, 5000)
 				setNotes(notes.filter((n) => n.id !== id))
 			})
 	}
@@ -52,6 +77,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<div>
 				<button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
 			</div>
@@ -64,6 +90,7 @@ const App = () => {
 				<input value={newNote} onChange={handleNoteChange} />
 				<button type='submit'>save</button>
 			</form>
+			<Footer />
 		</div>
 	)
 }
