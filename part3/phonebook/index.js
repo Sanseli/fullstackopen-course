7a59753bc230f8d5ee8bd051eb0a2fc9c56a128a
Sanseli <lisasansen97@gmail.com>
2023-02-13 16:56:28 +0200

    3.8*: Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', function (req, res) {
+	return JSON.stringify(req.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
 	{
@@ -68,6 +72,7 @@ app.post('/api/persons', (request, response) => {
 
 	persons = persons.concat(person)
 
+	morgan.token('body', (request) => JSON.stringify(request.body))
 	response.json(person)
 })
 
