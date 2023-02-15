da1b58b14a28d0365a24aaa64c2634adb89466ae
Sanseli <lisasansen97@gmail.com>
2023-02-15 18:31:33 +0200

    3.14: Phonebook database, step2

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -36,22 +36,18 @@ app.post('/api/persons', (request, response) => {
 		return response.status(400).json({
 			error: 'name of number is missing',
 		})
-	} else if (persons.find((person) => person.name === body.name)) {
-		return response.status(400).json({
-			error: 'name must be unique',
-		})
 	}
 
-	const person = {
-		id: Math.floor(Math.random() * 100),
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-	}
-
-	persons = persons.concat(person)
+	})
 
 	morgan.token('body', (request) => JSON.stringify(request.body))
-	response.json(person)
+
+	person.save().then((savedNote) => {
+		response.json(savedNote)
+	})
 })
 
 app.delete('/api/persons/:id', (request, response) => {
