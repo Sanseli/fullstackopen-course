62e9e01942a25c6c7dc6daa947a8bee6af893a43
Sanseli <lisasansen97@gmail.com>
2023-02-15 18:27:43 +0200

    3.13: Phonebook database, step1

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/persons')
 
 morgan.token('body', function (req, res) {
 	return JSON.stringify(req.body)
@@ -10,46 +13,20 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
 
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-]
-
 app.get('/', (request, response) => {
 	response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then((persons) => {
+		response.json(persons)
+	})
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find((person) => person.id === id)
-
-	if (person) {
+	Person.findById(request.params.id).then((person) => {
 		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	})
 })
 
 app.post('/api/persons', (request, response) => {
@@ -84,7 +61,7 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
