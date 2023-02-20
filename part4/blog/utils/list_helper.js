f6b662f481e6f81fb811fcbdc326483fc7c8ab7d
Sanseli <lisasansen97@gmail.com>
2023-02-20 15:13:31 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -11,6 +11,14 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
