808483ba97755bb73ed3349d70cefa2162761d33
Sanseli <lisasansen97@gmail.com>
2023-02-21 17:26:09 +0200

    4.9: Blog list tests, step2

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -6,7 +6,12 @@ const api = supertest(app)
 test('correct amount of blog posts are returned', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(3)
-}, 100000)
+})
+
+test('unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
