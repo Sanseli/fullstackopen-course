aa503c3686d36cf33f79cd1c773e6986c9d6e4b6
Sanseli <lisasansen97@gmail.com>
2023-02-22 13:54:50 +0200

    4.10: Blog list tests, step3

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,16 +1,50 @@
 const supertest = require('supertest')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('correct amount of blog posts are returned', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(3)
+  expect(response.body).toHaveLength(6)
 })
 
 test('unique identifier property is named id', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('making a HTTP POST request successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: 'New blog',
+    author: 'Test',
+    likes: 0,
+    url: 'url'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await helper.blogsInDb()
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+
+  const blogContents = blogs.map(n => n.title)
+  expect(blogContents).toContain(
+    'New blog'
+  )
 })
 
 afterAll(async () => {
