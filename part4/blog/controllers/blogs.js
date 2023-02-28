66763773dee9e5681def695fb37fd7faf49eae22
Sanseli <lisasansen97@gmail.com>
2023-02-28 19:34:51 +0200

    4.19: bloglist expansion, step7

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', (request, response, next) => {
   Blog
@@ -12,23 +13,45 @@ blogsRouter.get('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
-  if (!blog.likes) {
-    blog.likes = 0
+  const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
-  blog
-    .save()
-    .then(result => {
-      User.findById(blog.user).then((user) => {
+  User.findById(decodedToken.id).then((user) => {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.author,
+      likes: body.likes,
+      user: user.id
+    })
+
+    if (!blog.likes) {
+      blog.likes = 0
+    }
+
+    blog
+      .save()
+      .then(result => {
         user.blogs = user.blogs.concat(result._id)
         user.save().then(() => {
           response.status(201).json(result)
         })
       })
-    })
-    .catch(error => next(error))
+      .catch(error => next(error))
+  })
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
